2ea0558cf4c2f5ddd96be01399f279933cc4f24d
jf <jdtrashmail4@gmail.com>
2024-02-15 17:03:34 -0500

    update test position

---
+++ tests/test_posititon.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+from levelup.position import Position
+
+class TestPosition(TestCase):
+    def test_init(self):
+        x_pos = 5
+        y_pos = 5
+
+        TestPositionPosition = (x_pos, y_pos)
+        
