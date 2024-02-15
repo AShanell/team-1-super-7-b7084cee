8f13c4d22a0e11164e049a808b347483192894b0
jd <jdtrashmail4@gmail.com>
2024-02-15 18:24:56 -0500

    update

---
+++ src/levelup/map_double.py
@@ -0,0 +1,23 @@
+from levelup.position import Position
+from typing import Tuple
+class Map:
+    double_starting_position = Position()
+    double_positions = []
+    double_size: Tuple[int, int] = (10, 10)
+
+    # Exists for easy testing
+    num_positions = size[0]*size[1]
+
+    def __init__(self):
+        # TODO: implement method here and remove the print statement below
+        print("map constructor method not yet implemented")
+
+    def is_position_valid(self, position :Position) -> bool:
+        # TODO: implement method here and remove the print statement below
+        print("is_position_valid method not yet implemented")
+        return False        
+
+    def calculate_new_position(self, current_position: Position) -> Position:
+        # TODO: implement method here and remove the print statement below
+        print("calculate_new_position method not yet implemented")
+        return None
