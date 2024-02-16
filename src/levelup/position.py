1b1e4bbe1e960b6a1b7e3d37a59acaf2ddb1d29a
jd <jdtrashmail4@gmail.com>
2024-02-16 12:35:30 -0500

    commit test position

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,3 +1,3 @@
 class Position:
-    x_pos = range(1,10)
-    y_pos = range(1,10)
+    x_pos = int
+    y_pos = int
---
+++ tests/test_position.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from levelup.position import Position
+
+def checkRange(num):
+    if x_pos <=10 and y_pos <=10:
+        print ('Coordinates {} are in range')
+    else: 
+        print ('Coordinates {} are not in range')
+        
